b68b8389fc9ed07610af18159d9501ea80829e6d
Herve Delvaux <hde@idealisconsulting.com>
2022-04-05 00:58:08 +0200

    Added Create, Read controller routes

--- src/main/java/com/example/demo/controllers/AccountController.java
+++ src/main/java/com/example/demo/controllers/AccountController.java
@@ -1,6 +1,13 @@
 package banking;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 class AccountController {
@@ -9,6 +16,23 @@ class AccountController {
 
   AccountController(AccountService service) {
     this.service = service;
+  }
+
+  // ------- CRUD routes ------
+
+  @PostMapping("/accounts")
+  AccountDto newAccount(@RequestBody Account newAccount) {
+    return AccountDto.from(this.service.addAccount(newAccount));
+  }
+
+  @GetMapping("/accounts")
+  List<AccountDto> all() {
+    return this.service.getAccounts().stream().map(AccountDto::from).collect(Collectors.toList());
+  }
+
+  @GetMapping("/accounts/{id}")
+  AccountDto one(@PathVariable Long id) {
+    return AccountDto.from(this.service.getAccount(id));
   }
 
 }
--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,6 +1,13 @@
 package banking;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 class CustomerController {
@@ -9,6 +16,23 @@ class CustomerController {
 
   CustomerController(CustomerService service) {
     this.service = service;
+  }
+
+  // ------- CRUD routes ------
+
+  @PostMapping("/customers")
+  CustomerDto addCustomer(@RequestBody Customer newCustomer) {
+    return CustomerDto.from(this.service.addCustomer(newCustomer));
+  }
+
+  @GetMapping("/customers")
+  List<CustomerDto> getCustomers() {
+    return this.service.getCustomers().stream().map(CustomerDto::from).collect(Collectors.toList());
+  }
+
+  @GetMapping("/customers/{id}")
+  CustomerDto getCustomer(@PathVariable Long id) {
+    return CustomerDto.from(service.getCustomer(id));
   }
 
 }
