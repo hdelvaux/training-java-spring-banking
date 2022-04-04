03263e80297ee8b0459c1cc0b0d2ced5fd7d7b78
Herve Delvaux <hde@idealisconsulting.com>
2022-04-05 01:50:18 +0200

    Exposed endpoint from account perspective. It accepts an account with customerId and initialCredit data. If the initialCredit is set != 0, add a transaction from the Equity account

--- src/main/java/com/example/demo/controllers/AccountController.java
+++ src/main/java/com/example/demo/controllers/AccountController.java
@@ -1,6 +1,8 @@
 package banking;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 class AccountController {
@@ -11,4 +13,10 @@ class AccountController {
     this.service = service;
   }
 
+  // -------- Additional routes ---------
+
+    @PostMapping("/accounts/new")
+    AccountDto newAccount(@RequestBody NewAccountAccountDto newAccount) {
+     return AccountDto.from(this.service.newAccount(newAccount));
+    }
 }
--- src/main/java/com/example/demo/services/AccountService.java
+++ src/main/java/com/example/demo/services/AccountService.java
@@ -4,13 +4,19 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import banking.Account.AccountType;
+
 @Service
 public class AccountService {
 
   private AccountRepository repository;
+  private CustomerService customerService;
+  private TransactionService transactionService;
 
-  public AccountService(AccountRepository repository) {
+  public AccountService(AccountRepository repository, CustomerService customerService, TransactionService transactionService) {
     this.repository = repository;
+    this.customerService = customerService;
+    this.transactionService = transactionService;
   }
 
   // --------- CRUD methods ---------
@@ -27,4 +33,23 @@ public class AccountService {
     return this.repository.findById(id).orElseThrow(() -> new AccountNotFoundException(id));
   }
 
+  // -------- Additional methods --------------
+
+  public Account newAccount(NewAccountAccountDto newAccountDto){
+    Account account = new Account();
+    account.setName(newAccountDto.getName());
+    account.setType(AccountType.ASSET);
+
+    Customer customer = this.customerService.getCustomer(newAccountDto.getCustomerId());
+    customer.addAccount(account);
+    account.setCustomer(customer);
+    this.addAccount(account);
+    this.customerService.addCustomer(customer);
+
+    if(newAccountDto.getInitialCredit() != null && newAccountDto.getInitialCredit() != 0.0){
+        Transaction transaction = new Transaction(customer.getAccountsType(AccountType.EQUITY).get(0), account, newAccountDto.getInitialCredit());
+        this.transactionService.addTransaction(transaction);
+    }
+    return account;
+  }
 }
